e760322fd63e1226a4d0e5da2a976b005176e850
vinay mocharla <vinaymocharla8@gmail.com>
2020-07-13 20:09:56 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,flag=0;
+            num = Convert.ToInt16(Console.ReadLine());
+            for (int i = 2; i <= num/2; i++)
+            {
+               
+                if (num % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag==1)
+                {
+                    Console.WriteLine("Given no is not a prime no");
+                }
+                else if(flag==0)
+                {
+                    Console.WriteLine("given no is a prime no");
+                }
+
+            }
+        }
+    }
